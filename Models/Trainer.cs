4aea4183f5144b7a5d558f90b60ce782bd41d668
MarinaGribanova <m_gribanova33@mail.ru>
2023-05-11 17:35:01 +0300

    fix

--- Models/Trainer.cs
+++ Models/Trainer.cs
@@ -1,6 +1,17 @@
-﻿namespace Gribanova_API.Models
+﻿using System.Reflection;
+
+namespace Gribanova_API.Models
 {
     public class Trainer
     {
+        public int  TrainerId { get; set; } 
+        public string TrainerFirstName { get; set; }
+        public string TrainerLastName { get; set; }
+        public string TrainerSpecialization { get; set; } 
+
+        public int TrainerwWorkExperience { get; set; }
+
+        public string TrainerEmail { get; set; }
+        public ICollection<Training> TrainerTraining { get; set; }
     }
 }
--- Models/Training.cs
+++ Models/Training.cs
@@ -2,5 +2,11 @@
 {
     public class Training
     {
+         public int TrainingId { get; set; } 
+        public string TrainingName { get; set; } 
+        public DateTime TrainingDate { get; set; }
+        public string TrainingDescription { get; set; }
+        public int TrainingDuration { get; set; }
+        public ICollection<Trainer> TrainingTrainers { get; set; }
     }
 }
